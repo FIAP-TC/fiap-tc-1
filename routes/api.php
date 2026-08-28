eb49d1fc4d3d22c5f8885bc55ffcbca69c629c6f
marcel.farias <marcel.farias@crmbonus.com>
2026-08-28 17:04:23 -0300

    change: ajustando o endpoint test para estado padrao

--- routes/api.php
+++ routes/api.php
@@ -27,7 +27,7 @@ use Illuminate\Support\Facades\Route;
 
 // Rota de teste (sem autenticação)
 Route::prefix('test')->group(function () {
-    Route::get('/', fn() => response()->json(['status' => 'success', 'mensagem' => 'Working finalmente!']));
+    Route::get('/', fn() => response()->json(['status' => 'success', 'mensagem' => 'Working']));
 });
 
 Route::prefix('service-orders')->group(function () {
